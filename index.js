addbdd8e48253cee10d23b6b4e3c98dc8a0c9bb3
DarlanDuarte <darlan.duarte.m@gmail.com>
2023-04-05 16:29:15 -0300

    Implementando os Partials do EJS no Projeto

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 //Estou dizendo para o express usar o EJS como View Engine do meu projeto
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Para utilizar os arquivos Staticos no Express
 // Arquivos Staticos são arquivos CSS e Imagens e outros
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/:nome/:lang', (req, res) =>{
-    /*  const nome = 'Darlan Duarte'
-    const lang = 'NodeJs' */
-    const nome = req.params.nome
-    const lang = req.params.lang
-    const exibirMsg = true
-    const produtos = [
-        {nome: 'Doritos', preço: 3.14},
-        {nome: 'Coca-Cola', preço: 5},
-        {nome: 'Leite', preço: 2},
-        {nome: 'Carne', preço: 8},
-        {nome: 'RedBull', preço: 12},
-    ]
-    res.render('index', {
-        nome: nome,
-        lang: lang,
-        empresa: 'CloudMobile',
-        inscritos: 50000,
-        msg: exibirMsg,
-        produtos,
-    })
-
-})
+app.get("/", (req, res) => {
+  res.render("index");
+}); 
 
 
-app.get('/perguntar', (req, res) =>{
-    res.render('perguntar')
-})
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
 
-
-app.listen('8080', () =>{
-    console.log('App Rodando')
-})
+app.listen("8080", () => {
+  console.log("App Rodando");
+});
